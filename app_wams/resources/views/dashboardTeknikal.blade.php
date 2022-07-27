3e1caf5644f3f3187aa3660260dd041587338b79
GODZ <81666705+GODZ217@users.noreply.github.com>
2022-07-27 10:19:27 +0700

    update elearning

--- app_wams/resources/views/dashboardTeknikal.blade.php
+++ app_wams/resources/views/dashboardTeknikal.blade.php
@@ -33,7 +33,7 @@
                   <h4>News</h4>
                 </div>
                 <div class="card-body">
-                  {{$qq}}
+                  
                 </div>
               </div>
             </div>
